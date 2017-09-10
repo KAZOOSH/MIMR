5d6528fe579f8c31f8843d467ffc3829fd49d3ff
feuer <feuer@feuerundbenzin.tk>
2017-09-10 14:45:40 +0200

    leds test

--- instruments/5_drehding/adruino/main/drehding/drehding.ino
+++ instruments/5_drehding/adruino/main/drehding/drehding.ino
@@ -1,23 +1,30 @@
+#include <WS2812.h>
+
 #define NUMBER_OF_SHIFT_CHIPS   3
-#define DATA_WIDTH   NUMBER_OF_SHIFT_CHIPS * 8
-#define PULSE_WIDTH_USEC   5
-#define POLL_DELAY_MSEC   3000
+#define DATA_WIDTH NUMBER_OF_SHIFT_CHIPS * 8
+#define PULSE_WIDTH_USEC 5
 #define BYTES_VAL_T unsigned long
 
-int ploadPin        = 8;
-//int clockEnablePin  = 9; 
-int dataPin         = 7;
-int clockPin        = 4;
+const int ploadPin  = 8; //blue
+const int dataPin   = 7; //yellow
+const int clockPin  = 4; //green
 
-BYTES_VAL_T pinValues;
-BYTES_VAL_T oldPinValues;
+const int redLedPin = 5;   //R + red
+const int yellowLedPin = 6 ; //R + yellow
 
-/* This function is essentially a "shift-in" routine reading the
- * serial Data from the shift register chips and representing
- * the state of those pins in an unsigned integer (or long).
-*/
-BYTES_VAL_T read_shift_regs()
-{
+
+
+const int supplyPwmPin = 9; //Supply PWM pin
+const int supplyAdcPin = 1; //Feedback Input
+
+const float supplyVoltage = 13.8; 	//Important: Always set DC input Voltage of Supply in Volt !!!
+static float SupplySetpoint = 0;	//output voltage
+
+
+WS2812 LED(1);
+cRGB ledColor;
+
+BYTES_VAL_T read_shift_regs() {
     long bitVal;
     BYTES_VAL_T bytesVal = 0;
 
@@ -50,40 +57,73 @@ BYTES_VAL_T read_shift_regs()
     return(bytesVal);
 }
 
-/* Dump the list of zones along with their current status.
-*/
-void display_pin_values()
-{
-    Serial.print("Pin States:\r\n");
-
-    for(int i = 0; i < DATA_WIDTH; i++)
-    {
-        Serial.print("  Pin-");
-        Serial.print(i);
-        Serial.print(": ");
-
-        if((pinValues >> i) & 1)
-            Serial.print("HIGH");
-        else
-            Serial.print("LOW");
-
-        Serial.print("\r\n");
-    }
-
-    Serial.print("\r\n");
+inline byte registerValuteToSwitchValue(BYTES_VAL_T regValue) {
+  //Serial.print("\nbegin:");
+  //Serial.print(regValue);
+  for(int i = 0; i < DATA_WIDTH; i++) {
+    //Serial.print(i);
+    if(!((regValue >> i) & 1)) return i;
+  }
+  return 0;
 }
 
 
-#define redLedPin 5
-#define yellowLedPin 6
+inline void SupplySetVoltage(int voltage) {
+    SupplySetpoint = voltage;
+    if(SupplySetpoint > 65.0) SupplySetpoint = 65.0;
+}
+inline void SupplyFeedbackLoop() {
+
+  const float quantum = (5.0 / 1023) * 20.5; // U / bits * voltage divider
+  
+  float feedback;
+  float error;
+  
+  static byte op=0;
+  byte duty;
+
+  
+  feedback = analogRead(supplyAdcPin) * quantum - supplyVoltage;
+  error = SupplySetpoint - feedback;
+
+  if(SupplySetpoint < 3.0) {
+    //min voltage
+    OCR1A = 255;
+    return;
+  }
+  
+  if(error > 0.0) {
+    op++;
+  } else if(error < 0) {
+    op--;
+  }
+  
+  duty = 255 - op;      //invert
+  if(duty < 9) duty = 9;  //safty
+  OCR1A = duty;     //set timer1 register 
+}
+
+
+
+inline void ledFlicker() {
+  const byte flicker[] = {180, 30, 89, 23, 255, 200, 90, 150, 60, 230, 180, 45, 90};
+  #define flickerCount (sizeof(flicker) / sizeof(byte))
+  
+  static byte co=0;
+  if(co >= flickerCount) co = 0;
+  analogWrite(redLedPin, flicker[co]);
+  analogWrite(yellowLedPin, flicker[co]);
+  co++;
+}
+
+
 
 
 void setup()
 {
     Serial.begin(9600);
 
-    /* Initialize our digital pins...
-    */
+	//init shift registers for switch
     pinMode(ploadPin, OUTPUT);
     //pinMode(clockEnablePin, OUTPUT);
     pinMode(clockPin, OUTPUT);
@@ -92,45 +132,82 @@ void setup()
     digitalWrite(clockPin, LOW);
     digitalWrite(ploadPin, HIGH);
 
-    /* Read in and display the pin states at startup.
-    */
+	/*
     pinValues = read_shift_regs();
     display_pin_values();
     oldPinValues = pinValues;
+	*/
 
-
+	
+	//init led pins
     pinMode(redLedPin, OUTPUT);
     pinMode(yellowLedPin, OUTPUT);
+	
+	//init supply pins
+	pinMode(9, OUTPUT);
+
+  //Adruino leonardo fast pwm mode usign TIMER1
+  TCCR1A = (1 << WGM10);
+  TCCR1B = (1 << WGM12);
+  TCCR1C = 0;
+
+  
+  TCCR1B |= (1 << CS10); // no prescaler - 62KHz
+  TCCR1A |= (1 << COM1A1);
+
+  LED.setOutput(13);
+  ledColor.b = 255; ledColor.g = 0; ledColor.r = 0; // RGB Value -> Blue
+  String a;
+  while(1) {
+
     
+    
+    Serial.print("\nR: ");
+    while(Serial.available() < 2) {}
+    a = Serial.readStringUntil('\n');
+    ledColor.r = a.toInt();
+    Serial.print(ledColor.r);
+
+    Serial.print("\nG: ");
+    while(Serial.available() < 2) {}
+    a = Serial.readStringUntil('\n');
+    ledColor.g = a.toInt();
+    Serial.print(ledColor.g);
+
+    Serial.print("\nB: ");
+    while(Serial.available() < 2) {}
+    a = Serial.readStringUntil('\n');
+    ledColor.b = a.toInt();
+    Serial.print(ledColor.b);
+    
+    LED.set_crgb_at(0, ledColor); // Set value at LED found at index 0
+    LED.sync(); // Sends the value to the LED
+    delay(500);  
+  }
 }
 
-byte flicker[] = {180, 30, 89, 23, 255, 200, 90, 150, 60, 230, 180, 45, 90};
-#define flickerCount (sizeof(flicker) / sizeof(byte))
 
-void loop()
-{
-  
-    /* Read the state of all zones.
-    */
-    pinValues = read_shift_regs();
+void loop() {
+	
+	BYTES_VAL_T pinValues;
+	BYTES_VAL_T oldPinValues;
+	byte sw;
+	while(1) {
+		pinValues = read_shift_regs();
+		if(pinValues != oldPinValues) {
+			Serial.print("*Pin value change detected*\r\n");
+			sw = registerValuteToSwitchValue(pinValues);
+     
+      Serial.print(sw);
+			SupplySetVoltage(sw*3);
+			oldPinValues = pinValues;
+		}
 
-    /* If there was a chage in state, display which ones changed.
-    */
-    if(pinValues != oldPinValues)
-    {
-        Serial.print("*Pin value change detected*\r\n");
-        display_pin_values();
-        oldPinValues = pinValues;
-    }
-
-
-    static byte co=0;
-    if(co >= flickerCount) co = 0;
-    
-    analogWrite(redLedPin, flicker[co]);
-    analogWrite(yellowLedPin, flicker[co]);
-    co++;
-
+		SupplyFeedbackLoop();
+		
+		//led flicker efect
+		ledFlicker();
+	}
     //delay(POLL_DELAY_MSEC);
 }
 
--- instruments/5_drehding/adruino/pwm-test/pwm-test.ino
+++ instruments/5_drehding/adruino/pwm-test/pwm-test.ino
@@ -1,35 +1,212 @@
-/*
-void setPwmFrequency(int pin, int divisor) {
-  byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 64: mode = 0x03; break;
-      case 256: mode = 0x04; break;
-      case 1024: mode = 0x05; break;
-      default: return;
-    }
-    if(pin == 5 || pin == 6) {
-      TCCR0B = TCCR0B & 0b11111000 | mode;
-    } else {
-      TCCR1B = TCCR1B & 0b11111000 | mode;
-    }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 32: mode = 0x03; break;
-      case 64: mode = 0x04; break;
-      case 128: mode = 0x05; break;
-      case 256: mode = 0x06; break;
-      case 1024: mode = 0x07; break;
-      default: return;
-    }
-    TCCR2B = TCCR2B & 0b11111000 | mode;
+
+
+void test1() {
+  int adc;
+  for(int co=255;co > 0;co--) {
+    OCR1A = co;
+
+    delay(100);
+    adc = analogRead(1);
+    Serial.print(co);
+    Serial.print(" ");
+    Serial.print(adc);
+    Serial.print('\n');
   }
 }
-*/
+
+void test2() {
+  const double quantum = 5.0 / 1023;
+  int adc0;
+  int adc1;
+  byte duty = 0;
+  while(1) {    
+    adc1 = analogRead(1);
+
+    double voltage1 = adc1 * quantum;
+    double voltage2 = adc1 * quantum * 20.5;
+
+    
+  
+    adc0 = analogRead(0);
+
+    duty = adc0 / 4;
+    OCR1A = duty;
+
+    Serial.print(adc1);
+    Serial.print(" ");
+    Serial.print(voltage1);    
+    Serial.print(" ");
+    Serial.print(voltage2);
+    Serial.print(" ");
+    Serial.print(adc0);
+    Serial.print(" ");
+    Serial.print(duty);
+    Serial.print("\n");       
+  }  
+}
+
+
+void test3() {
+
+  const float supply = 13.8;
+  const float quantum = (5.0 / 1023) * 20.5;
+
+ 
+  int adc0;
+  int adc1;
+  
+  
+  float setpoint;
+  float feedback;
+  float error;
+  
+  int change;
+  byte op=0;
+  byte duty;
+  
+  while(1) {
+    adc0 = analogRead(0);//poti
+    adc1 = analogRead(1);//divider
+
+    
+    setpoint = (adc0 / 8);
+
+    if(setpoint > 65.0) setpoint = 65.0;
+    
+    feedback = adc1 * quantum - supply;
+    error = setpoint - feedback;
+
+    if(error > 0) {
+      op++;
+      change = 1;
+    } else if(error < 0) {
+      op--;
+      change = -1;
+    }
+
+    
+    duty = 255 - op;    //invert
+    if(duty < 9) duty = 9; //safty
+    OCR1A = duty;
+    
+
+
+
+    Serial.print("adc0: ");
+    Serial.print(adc0);
+    Serial.print(" adc1: ");
+    Serial.print(adc1);
+
+    Serial.print(" set: ");
+    Serial.print(setpoint);
+    Serial.print(" fedback: ");
+    Serial.print(feedback);
+    Serial.print(" error: ");
+    Serial.print(error);
+        
+    Serial.print(" change: ");
+    Serial.print(change);
+    Serial.print(" duty: ");
+    Serial.print(duty);
+    Serial.print("\n");
+  }  
+
+
+}
+
+
+
+
+void test4() {
+
+  const float supply = 13.8;
+  const float quantum = (5.0 / 1023) * 20.5;
+
+  const float Kp = 4;
+  const float Ki = 0.0;
+  const float Kd = 0.0;
+
+  static float P=0,I=0,D=0;
+  float PID;
+
+
+
+  
+  int adc0;
+  int adc1;
+  
+  
+  float setpoint;
+  float feedback;
+  float Lfeedback;
+  float error;
+  
+  int change;
+
+  byte duty;
+  
+  while(1) {
+    adc0 = analogRead(0);//poti
+    adc1 = analogRead(1);//divider
+
+    
+    setpoint = (adc0 / 8);
+    feedback = adc1 * quantum - supply;
+    error = setpoint - feedback;
+
+    P = Kp * error;
+    I += Ki * error;
+    D = Kd * (Lfeedback - feedback);
+
+    Lfeedback = feedback;
+    
+    PID = P + I + D;
+    
+    change = PID;
+    duty = PID;
+    if(duty > 200) duty = 200;
+
+    OCR1A = 255 - duty;
+    //OCR1A = 150;
+
+
+
+    Serial.print("adc0: ");
+    Serial.print(adc0);
+    Serial.print(" adc1: ");
+    Serial.print(adc1);
+
+    Serial.print(" set: ");
+    Serial.print(setpoint);
+    Serial.print(" fedback: ");
+    Serial.print(feedback);
+    Serial.print(" error: ");
+    Serial.print(error);
+
+
+    Serial.print(" P: ");
+    Serial.print(P);
+    Serial.print(" I: ");
+    Serial.print(I);
+    Serial.print(" D: ");
+    Serial.print(D);
+    Serial.print(" PID: ");
+    Serial.print(PID);
+        
+    Serial.print(" change: ");
+    Serial.print(change);
+    Serial.print(" duty: ");
+    Serial.print(duty);
+    Serial.print("\n");
+  }  
+
+
+}
+
+
+
+
+
 void setup() {
     Serial.begin(9600);
     pinMode(9, OUTPUT);
@@ -47,16 +224,13 @@ void setup() {
     //TCCR1B |= (1 << CS11); // no prescaler - 62KHz
 
     TCCR1A |= (1 << COM1A1);
-   
+
+   //test1();
 }
 
-int adc;
-void loop() {
 
-   
-  adc = analogRead(0);
-  //analogWrite(6,adc / 4);
-  OCR1A = (adc / 4);
-  Serial.print(adc);
-  Serial.print("\n");
+void loop() {
+  //test1();
+  //test2();
+  test3();
 }
